e9a7a05779d868dccb8a6a4d78e70c1b1d1f4a4c
HowardHarrison <peterthureain@gmail.com>
2024-02-07 20:56:18 +0630

    voice cleancode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 
-export default function Header() {
+export default function Header({ voice, onChange}) {
     const [darkMode, setDarkMode] = useState(false);
-    const [voice, changeVoice] = useState(true);
     
     useEffect(() => {
      const body = document.body;
@@ -14,8 +13,6 @@ export default function Header() {
      }
     }, [darkMode]);
 
-    
-
     return (
         <div className="flex justify-between bg-blue-500">
          <h1 className="text-2xl font-bold text-dark-200 m-6 ">
@@ -23,7 +20,7 @@ export default function Header() {
          </h1>
          <div className="text-3xl font-bold text-dark-200 m-6 flex">
           <span className="px-3">
-           <button onClick={() => changeVoice(!voice)}>
+           <button onClick={onChange}>
             {voice ? 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
